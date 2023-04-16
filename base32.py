3e8dcb0b02c7f672cc5b2314f711fc154537b7f0
Ardit Haklaj <118832945+ArditHaklaj@users.noreply.github.com>
2023-04-16 20:54:20 +0200

    Update base32.py

--- base32.py
+++ base32.py
@@ -1,4 +1,3 @@
-//Arbnora
 import base64
 def base32_encode(string):
     """Encodes a string to Base32"""
@@ -6,22 +5,15 @@ def base32_encode(string):
     encoded_bytes = base64.b32encode(string_bytes)
     encoded_string = encoded_bytes.decode('utf-8')
     return encoded_string
-
-
-
-
-
-
-//Xhemajli
 def base32_decode(encoded_string):
     """Decodes a Base32-encoded string"""
     encoded_bytes = encoded_string.encode('utf-8')
     decoded_bytes = base64.b32decode(encoded_bytes)
     decoded_string = decoded_bytes.decode('utf-8')
     return decoded_string
-//Arditi
+
 encoded_string = base32_encode("Ky eshte projekti i dyte")
 print(encoded_string)
-//Erblini
+
 decoded_string = base32_decode(encoded_string)
 print(decoded_string)
