c334e13b89d3a973512dd30a5d4cca26f43b1e03
xXhemajl <115585034+xXhemajl@users.noreply.github.com>
2023-04-16 20:36:53 +0200

    Update base32.py

--- base32.py
+++ base32.py
@@ -1,1 +1,14 @@
 //insertone kodin qetu
+
+
+
+
+
+
+//Xhemajli
+def base32_decode(encoded_string):
+    """Decodes a Base32-encoded string"""
+    encoded_bytes = encoded_string.encode('utf-8')
+    decoded_bytes = base64.b32decode(encoded_bytes)
+    decoded_string = decoded_bytes.decode('utf-8')
+    return decoded_string
