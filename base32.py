0888010b3bc92c2b27b3c257a2e8416293b69ed5
Ardit Haklaj <118832945+ArditHaklaj@users.noreply.github.com>
2023-04-16 20:38:55 +0200

    Update base32.py

--- base32.py
+++ base32.py
@@ -12,3 +12,6 @@ def base32_decode(encoded_string):
     decoded_bytes = base64.b32decode(encoded_bytes)
     decoded_string = decoded_bytes.decode('utf-8')
     return decoded_string
+//Arditi
+encoded_string = base32_encode("Ky eshte projekti i dyte")
+print(encoded_string)
